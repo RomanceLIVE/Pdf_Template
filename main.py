e703773695b5a735148ed8d6f739e9735d1ccd09
Robert Horvat <robert.horvath93@gmail.com>
2023-09-13 00:29:28 +0300

    edits

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 # this is a python object (class type)
-#page orientation portrait, milimereters and A4 format
+# page orientation portrait, milimereters and A4 format
+pdf.set_auto_page_break(auto=False, margin=0)
+# we set the page to NOT break
+
 
 df = pd.read_csv("topics.csv")
 
@@ -21,8 +24,21 @@ for index, row in df.iterrows():
     pdf.line(10, 21, 200, 21)
             #x1,  y1,  x2,  y2 line values, which are 2D line distances of trajectory
 
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+    # we set the footer pdf.ln()
+
     for i in range(row["Pages"]-1):
         pdf.add_page()
+
+        pdf.ln(277)
+        # we did 265 + w=21  = 277
+        # due to vertical reference (page title) being blank in new pages
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     # we create a nested for loop to generate
     # the nr of pages mentioned in the pages column
